aead31d686eefc7b2712d1180208a9e2cd1bd1c7
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2020-12-18 16:19:25 -0500

    Set the preview node in the update tree node ScUpdateTree

--- nodes/utilities/ScUpdateTree.py
+++ nodes/utilities/ScUpdateTree.py
@@ -1,6 +1,6 @@
 import bpy
 
-from bpy.props import PointerProperty
+from bpy.props import PointerProperty, StringProperty
 from bpy.types import Node
 from .._base.node_base import ScNode
 from ...debug import log
@@ -11,10 +11,12 @@ class ScUpdateTree(Node, ScNode):
     bl_icon = 'SCENE_DATA'
 
     prop_nodetree: PointerProperty(name="NodeTree", type=bpy.types.NodeTree, update=ScNode.update_value)
+    in_node_name: StringProperty(name="Node name", update=ScNode.update_value)
     
     def init(self, context):
         super().init(context)
         self.inputs.new("ScNodeSocketUniversal", "In")
+        self.inputs.new("ScNodeSocketString", "Node name").init("in_node_name", True)
         self.outputs.new("ScNodeSocketUniversal", "Out")
     
     def draw_buttons(self, context, layout):
@@ -25,16 +27,18 @@ class ScUpdateTree(Node, ScNode):
         return (
             super().error_condition()
             or self.prop_nodetree == None
+            or self.inputs["Node name"].default_value == ""
         )
     
     def functionality(self):
         # Rename tree for convenience
         curr_tree = self.prop_nodetree
+        node_name = self.inputs["Node name"].default_value
 
         log(self.id_data.name, self.name, "functionality", "Update tree: " + curr_tree.name)
         
         # Update the view
-        curr_tree.execute_node()
+        curr_tree.set_preview(node_name)
     
     def post_execute(self):
         out = super().post_execute()
