9ab2248298a391401d72330e292ac814639b6ec5
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2020-12-18 23:57:16 -0500

    ScOptimizeTree takes arrays as input

--- nodes/utilities/ScOptimizeTree.py
+++ nodes/utilities/ScOptimizeTree.py
@@ -7,6 +7,7 @@ from .._base.node_base import ScNode
 from ...optimization.ScInverseModelingSolver import ScInverseModelingSolver
 from ...optimization.ScOrientedBoundingBox import ScOrientedBoundingBox
 from ...debug import log
+from ...helper import convert_data
 
 class ScOptimizeTree(Node, ScNode):
     bl_idname = "ScOptimizeTree"
@@ -14,14 +15,12 @@ class ScOptimizeTree(Node, ScNode):
     bl_icon = 'CON_TRANSFORM_CACHE'
 
     prop_nodetree: PointerProperty(name="NodeTree", type=bpy.types.NodeTree, update=ScNode.update_value)
-    in_target_name: StringProperty(name="Target name", update=ScNode.update_value)
-    in_center: FloatVectorProperty(name="Center", update=ScNode.update_value)
     
     def init(self, context):
         super().init(context)
         self.inputs.new("ScNodeSocketUniversal", "In")
-        self.inputs.new("ScNodeSocketString", "Target name").init("in_target_name", True)
-        self.inputs.new("ScNodeSocketVector", "Center").init("in_center", True)
+        self.inputs.new("ScNodeSocketArray", "Targets")
+        self.inputs.new("ScNodeSocketArray", "Centers")
         self.outputs.new("ScNodeSocketUniversal", "Out")
     
     def draw_buttons(self, context, layout):
@@ -31,28 +30,42 @@ class ScOptimizeTree(Node, ScNode):
     def error_condition(self):
         return (
             super().error_condition()
-            or self.prop_nodetree == None
-            or self.inputs["Target name"].default_value == ""
+            or self.prop_nodetree == None            
         )
     
     def functionality(self):
-        # Rename tree for convenience
+        # Rename variables for convenience
         curr_tree = self.prop_nodetree
-        target_name = self.inputs["Target name"].default_value
-        center = mathutils.Vector(self.inputs["Center"].default_value)
+        target_names = eval(self.inputs["Targets"].default_value)
+        centers = eval(self.inputs["Centers"].default_value)
 
         log(self.id_data.name, self.name, "functionality", "Optimize tree: " + curr_tree.name)
 
+        # If target_names is just a string, make it an array
+        if not isinstance(target_names, list):
+            target_names = [target_names]
+
+        # If centers is just a vector, make it an array
+        if not isinstance(centers, list):
+            centers = [centers]
+
         # Setup target for optimization
         target_bounding_boxes = {}
-        # Get the current bounding box from the tree
         tree_boxes = curr_tree.get_object_boxes()
-        if target_name in tree_boxes:
-            target_bounding_boxes[target_name] = tree_boxes[target_name]
-        else:
-            target_bounding_boxes[target_name] = ScOrientedBoundingBox.defaultBoundingBox()
-        # Modify the bounding box accroding to the input to this node
-        target_bounding_boxes[target_name].center = center
+
+        for i in range(min(len(target_names), len(centers))):
+            target_name = target_names[i]
+            # Convert the center to a Vector
+            (converted, center_tuple) = convert_data(centers[i], from_type="STRING", to_type="VECTOR")
+            if converted:
+                center = mathutils.Vector(center_tuple)
+                # Get the current bounding box from the tree
+                if target_name in tree_boxes:
+                    target_bounding_boxes[target_name] = tree_boxes[target_name]
+                else:
+                    target_bounding_boxes[target_name] = ScOrientedBoundingBox.defaultBoundingBox()
+                # Modify the bounding box accroding to the input to this node
+                target_bounding_boxes[target_name].center = center
 
         # Optimization
         initial_float_properties = curr_tree.get_float_properties()
