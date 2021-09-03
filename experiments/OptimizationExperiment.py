fd2c7d664b8b8ff02986b5e6b61c91585c7f5e46
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2021-09-03 15:21:36 -0400

    Starting the Python optimization experiment

---
+++ experiments/OptimizationExperiment.py
@@ -0,0 +1,15 @@
+import scipy
+
+# TODO: target function to optimize
+# TODO: optimize with SciPy
+# TODO: measure optimization time
+# TODO: record path of the optimization
+# TODO: plot the 2D function 
+# TODO: plot the optimization path
+# TODO: implement the benchmark function with CasADi and use AD for Jacobian and Hessian
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
