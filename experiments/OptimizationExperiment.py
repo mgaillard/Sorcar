21f9888e7fba5284a8b36ea50e6597cb67e82a10
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2021-09-05 22:53:24 -0400

    Add global optimization algorithm Add new functions to test global optimization

--- experiments/OptimizationExperiment.py
+++ experiments/OptimizationExperiment.py
@@ -1,13 +1,18 @@
+import math
 from timeit import default_timer
 import numpy as np
 from casadi import *
-from scipy.optimize import minimize, rosen, rosen_der
+from scipy.optimize import minimize, basinhopping
 import matplotlib.pyplot as plt
 
 # TODO: animation of the plot
-# TODO: implement a list for function optimization, and minize all of them with different optimizers
+# TODO: implement a list for function optimization, and minimize all of them with different optimizers
+# TODO: add undetermined functions with a solution set that is 1D and curved
+# TODO: add undetermined functions with a solution set that is 1D and circular
+# TODO: add undetermined function with a solution set that is 2D
 # TODO: add global optimization objective function
 # TODO: add global optimization methods
+# TODO: look at the Hessian of the underdetermined function in the valley
 
 class OptimizationHistory:
     """ Register evaluations of the cost function as optimization is happening """
@@ -28,6 +33,15 @@ class OptimizationHistory:
         """
         # Note that we make a deep copy of xk
         self.history.append(np.copy(xk))
+
+    def add_sample_basinhopping(self, x, f, accept):
+        """
+        Callback for scipy.optimize.basinhopping()
+        Parameter x is the current parameter vector
+        We ignore other parameters
+        """
+        # Note that we make a deep copy of xk
+        self.history.append(np.copy(x))
 
 
 class CasadiFunction:
@@ -99,7 +113,7 @@ def generate_rosen():
     Optimimum: [1.0, 1.0]
     """
     x = SX.sym('x', 2)
-    expr = (1.0 - x[0])*(1.0 - x[0]) + 100.0*(x[1] - x[0]*x[0])*(x[1] - x[0]*x[0])
+    expr = pow(1.0 - x[0], 2) + 100.0*pow(x[1] - x[0]*x[0], 2)
     return CasadiFunction(expr, x)
 
 
@@ -112,7 +126,32 @@ def generate_underdetermined_linear():
     # Function
     z = x[0] + x[1]
     # We want to fit z so that z = 1.0
-    expr = (z - 1.0)*(z - 1.0)
+    expr = pow(z - 1.0, 2)
+    return CasadiFunction(expr, x)
+
+
+def generate_himmelblau():
+    """
+    Himmelblau function
+    (x^2 + y - 11)^2 + (x + y^2 - 7)^2
+    Has four local minima (up to 1 decimal place):
+     * 3.0, 2.0
+     * -2.8, 3.1
+     * -3.8, -3.3
+     * 3.6, -1.8
+    """
+    x = SX.sym('x', 2)
+    expr = pow(x[0]*x[0] + x[1] - 11.0, 2) + pow(x[0] + x[1]*x[1] - 7.0, 2)
+    return CasadiFunction(expr, x)
+
+
+def generate_rastrigin():
+    """
+    Rastigrin function
+    20.0 + (x^2 - 10.0*cos(2*pi*x)) + (y^2 - 10.0*cos(2*pi*y))
+    """
+    x = SX.sym('x', 2)
+    expr = 20.0 + pow(x[0]*x[0] - 10.0*cos(2.0*math.pi*x[0]), 2) + pow(x[1]*x[1] - 10.0*cos(2.0*math.pi*x[1]), 2)
     return CasadiFunction(expr, x)
 
 
@@ -120,7 +159,9 @@ def generate_functions():
     """ Generate a list of predefined functions to optimize """
     functions = {
         'rosen': generate_rosen(),
-        'underdetermined_linear': generate_underdetermined_linear()
+        'underdetermined_linear': generate_underdetermined_linear(),
+        'himmelblau': generate_himmelblau(),
+        'rastrigin': generate_rastrigin()
     }
     return functions
 
@@ -135,8 +176,10 @@ def plot_function_contour_with_samples(function, path):
     resolutionX = 50
     resolutionY = 50
 
-    x = np.linspace(-2.0, 2.0, resolutionX)
-    y = np.linspace(-1.0, 3.0, resolutionY)
+    # x = np.linspace(-2.0, 2.0, resolutionX)
+    # y = np.linspace(-1.0, 3.0, resolutionY)
+    x = np.linspace(-5.0, 5.0, resolutionX)
+    y = np.linspace(-5.0, 5.0, resolutionY)
     X, Y = np.meshgrid(x, y)
 
     Z = np.zeros((resolutionX, resolutionY))
@@ -183,10 +226,29 @@ def optimization(function):
     plot_function_contour_with_samples(function, optim_history.get_history())
 
 
+def global_optimization(function):
+    x0 = [1.3, 0.7]
+    optim_history = OptimizationHistory()
+    optim_history.add_sample(x0)
+
+    start_time = default_timer()
+    minimizer_kwargs = {"method": "BFGS", "jac": function.derivative}
+    res = basinhopping(function.evaluate,
+                       x0,
+                       minimizer_kwargs=minimizer_kwargs,
+                       niter=200,
+                       callback=optim_history.add_sample_basinhopping)
+    end_time = default_timer()
+    
+    print('Optimization time: {} s'.format(end_time - start_time))
+    print('Final parameter vector: {}'.format(res.x))
+    plot_function_contour_with_samples(function, optim_history.get_history())
+
+
 def main():
     functions = generate_functions()    
     # Optimization of the function
-    optimization(functions['underdetermined_linear'])
+    optimization(functions['rosen'])
 
 
 if __name__ == "__main__":
